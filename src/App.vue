8a628ed3f667364aa1b8a2716730c4c27569c7da
Rogertheone <ln451920@dal.ca>
2025-03-04 17:17:02 -0400

    Init commit set up checkmate vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template> 
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader :receive="receive"/>
+        <MyList :todos="todos" :remove="remove" :deleteTodo="deleteTodo"/>
+        <MyFooter :todos="todos" :checkAlltodo="checkAlltodo"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from "./components/MyHeader.vue";
+import MyFooter from "./components/MyFooter.vue";
+import MyList from "./components/MyList.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: { MyHeader, MyFooter, MyList },
+  data(){
+    return{
+       todos:[
+         {id:'001',title:'Go Bathing', completed:'true' },
+         {id:'002',title:'Go Sleeping', completed:'true' },
+         {id:'003',title:'Go Shopping', completed:'true' },
+       ]
+    }
+  },
+  methods:{
+    receive(todo){
+      this.todos.unshift(todo)
+    },
+    remove(id){
+      this.todos.forEach((todo)=>{
+        if (todo.id === id) todo.completed = !todo.completed
+      })
+    },
+    deleteTodo(id){
+      this.todos = this.todos.filter((todo)=>{
+        return todo.id !== id
+      })
+    },
+    checkAlltodo(completed){
+      this.todos.forEach((todo)=>{
+        todo.completed = completed 
+      })
+    
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+/*base*/
+body {
+  background: #fff;
+}
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+.btn:focus {
+  outline: none;
+}
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
 }
 </style>
---
+++ src/components/MyFooter.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="todo-footer" v-show="todos.length">
+    <label>
+      <input type="checkbox" :checked="getTotal === todos.length " @change="checkAll"/>
+    </label>
+    <span> <span>Completed: {{getTotal}}</span> / All: {{todos.length}} </span>
+    <button class="btn btn-danger">Clear Completed Tasks</button>
+  </div> 
+</template>
+  
+<script>
+export default {
+  name: 'MyFooter',
+  props:['todos','checkAlltodo'],
+  computed:{
+    getTotal(){
+      return this.todos.reduce((pre,current) => pre + (current.completed? 1:0) ,0)
+    }
+  },
+  methods:{
+    checkAll(e){
+      this.checkAlltodo(e.target.checked)
+    }
+    
+  }
+};
+</script>
+
+<style scoped>
+/*footer*/
+.todo-footer {
+  height: 40px;
+  line-height: 40px;
+  padding-left: 6px;
+  margin-top: 5px;
+}
+
+.todo-footer label {
+  display: inline-block;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.todo-footer label input {
+  position: relative;
+  top: -1px;
+  vertical-align: middle;
+  margin-right: 5px;
+}
+
+.todo-footer button {
+  float: right;
+  margin-top: 5px;
+}
+</style>
